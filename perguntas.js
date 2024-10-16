be4ce30d5b079640227979faf33b4a1e7e8d1d88
123 <163301567+HanyeGomes10@users.noreply.github.com>
2024-10-16 09:09:34 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    'QUÍMICA',
+    'De quem era o modelo atômico chamado de "bola de bilhar?"',
+    'O modelo foi proposto por John Dalton em 1808.'
+)
+
+criaCartao(
+    'QUÍMICA',
+    'Thomson propôs qual modelo atômico?',
+    'Thomson propôs o modelo atômico conhecido como modelo o "pudim de passas" em 1889.'
+)
+
+criaCartao(
+    'QUÍMICA',
+    'Quais são as principais características do modelo "planetário" proposto por Rutherford em 1911?',
+    'Eletrosfera, núcleo denso e positivo e a emissão de partícolas gama em lâminas de ouro.'
+)
+
+criaCartao (
+    'QUÍMICA',
+    'Quantos níveis de energia existem no modelo de Bohr proposto em 1913?',
+    'O modelo proposto apresenta 7 camadas de energia, sendo elas: K, L, M, N, O, P, Q.'
+)
+
+criaCartao (
+    'QUÍMICA',
+    'O que diz a regra do octeto?',
+    'Ela diz que um átomo só ganha estabilidade com 8 elétrons na camada de valência.'
+)
+
+criaCartao (
+    'QUÍMICA',
+    'Principais características das ligações iônicas?',
+    'Alto ponto de ebulição e fusão geralmente, sólidas e duras e quebradiças.'
+)
+
+criaCartao (
+    'QUÍMICA',
+    'Principais características das ligações metálicas?',
+    'Bons condutores de energia e de calor, ducteis e maleáveis, sólidos em temperatura ambiente, alto ponto de fusão e ebulição.'
+)
+
+criaCartao (
+    'QUÍMICA',
+    'Quais são as 7 formas da geometria molecular?',
+    'Linear, Angular, Trigonal, Piramidal, Tetraédrica, Bipiramidal e Octaédrica'
+)
+
+criaCartao (
+    'QUÍMICA',
+    'O que é massa molar?',
+    'É a maneira utilizada para mensurar a massa de uma única molécula, calculada pela soma das massas de todos os átomos presentes na molécula.'
+)
